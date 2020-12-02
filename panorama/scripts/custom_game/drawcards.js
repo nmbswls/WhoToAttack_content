cdc60bd784a0d723e71354701ceae788da0c2fbd
nmbswls <nmbswls@163.com>
2020-12-02 20:44:42 +0800

    Update drawcards.js
    modify

--- panorama/scripts/custom_game/drawcards.js
+++ panorama/scripts/custom_game/drawcards.js
@@ -19,3 +19,62 @@ function SetCardAbilityPanelEvent(panel,abilityName) {
         $.DispatchEvent("DOTAHideAbilityTooltip");
    })    
 }
+
+function OnShowCards(keys){
+	$('#hand_cards').text = keys.hand_cards;
+    
+    var names = keys.hand_cards.split(',')
+    var cardSelectionPanel = $("#CardSelection_Body");
+    
+    
+    for (var k in names) {
+        var name = names[k];
+        if(name == null || name == "")continue;
+        $.Msg("names: " + name);
+        var panelID = "card_"+k;
+        var panel = cardSelectionPanel.FindChildTraverse(panelID);
+        if (panel == undefined && panel == null) {
+            panel = $.CreatePanel("Panel", cardSelectionPanel, panelID);
+            panel.BLoadLayoutSnippet("Card");
+            InitCardPanelEvent(panel);
+        }
+        //panel.FindChildTraverse("CardImage").SetImage("file://{images}/custom_game/card/"+heroName+".png");
+        panel.FindChildTraverse("CardName").text = $.Localize(name);
+        panel.idx =  k
+    }
+
+    $("#CardSelection").SetHasClass("show", true);
+    $("#CardSelection_Body").AddClass("draw");
+}
+
+function OnPingOpenDoors(keys) {
+    
+    var loc = [keys.x, keys.y, keys.z];
+    for(var i=0;i<3;i++){
+        $.Schedule(i*0.8, function(){
+            GameUI.PingMinimapAtLocation(loc);
+        });
+    }
+}
+
+function InitCardPanelEvent(panel) {
+    panel.SetPanelEvent("onactivate", function() {
+        
+        CardPicked(panel.idx)
+    });
+}
+
+function CloseCardSelection() {
+    $("#CardSelection").SetHasClass("show", false);
+    $("#CardSelection_Body").RemoveClass("draw");
+}
+
+function CardPicked(cardIdx){
+   
+    GameEvents.SendCustomGameEventToServer("PickCard", {
+        card_idx : cardIdx
+    });
+    $.Msg("clicked " + cardIdx);
+    $("#CardSelection").SetHasClass("show", false);
+    $("#CardSelection_Body").RemoveClass("draw");
+}
