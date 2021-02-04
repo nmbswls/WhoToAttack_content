754485cadf91f35ebe26abb6254df883afcd10bb
unknown <nmbswls@163.com>
2021-02-04 21:22:49 +0800

    shangcheng add

--- panorama/scripts/custom_game/shop.js
+++ panorama/scripts/custom_game/shop.js
@@ -6,6 +6,9 @@ var m_SlotEquipInfo = {};
 
 var m_PlayerCollectionData = {}
 
+var m_ShopLinesPanels = {}; 
+var SHOP_TAGS = ["tab1","tab2","tab3","tab4"]
+
 function OpenShop(){
     $("#page_shop").ToggleClass("Hidden");
     //$("#menu_hit_listener").RemoveClass("MenuHidden");
@@ -37,21 +40,32 @@ function CloseShop(){
 
 
 function UpdateShopItems() {
-    var parent = $("#shop_items");
     var points = m_PointsLeft;
-    var childCount = parent.GetChildCount();
-
-    for (var i = 0; i < childCount; i++) {
-        var child = parent.GetChild(i)
-        var cost = child.cost;
+	
+	for (var k in m_ShopItemPanels) {
+		var panel = m_ShopItemPanels[k];
+		
+        var cost = panel.cost;
         if (cost > points) {
-            child.enabled = false;
-            child.AddClass("NotEnoughPoints");
+            panel.enabled = false;
+            panel.AddClass("NotEnoughPoints");
         }else{
-            child.enabled = true;
-            child.RemoveClass("NotEnoughPoints");
+            panel.enabled = true;
+            panel.RemoveClass("NotEnoughPoints");
         }
     }
+	
+    // for (var i = 0; i < childCount; i++) {
+        // var child = parent.GetChild(i)
+        // var cost = child.cost;
+        // if (cost > points) {
+            // child.enabled = false;
+            // child.AddClass("NotEnoughPoints");
+        // }else{
+            // child.enabled = true;
+            // child.RemoveClass("NotEnoughPoints");
+        // }
+    // }
 }
 
 function ShowConfirmPurchaseDialog(name, cost){
@@ -81,62 +95,102 @@ function HideConfirmPurchasePanel() {
     $("#confirm_purchase_dialog").AddClass("Hidden");
 }
 
+function InitTabContainers() {
+	var lineContainer = $("#shop_lines");
+	for(var tagName in SHOP_TAGS){
+		var tabName = SHOP_TAGS[tagName];
+		m_ShopLinesPanels[tabName] = $.CreatePanel("Panel", lineContainer, "");
+		m_ShopLinesPanels[tabName].BLoadLayoutSnippet("ShopLine");
+		m_ShopLinesPanels[tabName].FindChildTraverse("line_subtitle_text").text = $.Localize(tabName);
+	}
+}
+
 function BuildShopItems(data) {
-    var parent = $("#shop_items");
-    parent.RemoveAndDeleteChildren();
+	
+	for (var k in m_ShopLinesPanels) {
+		var panel = m_ShopLinesPanels[k].FindChildTraverse("shop_items");
+        panel.RemoveAndDeleteChildren();
+    }
+	
+	for (var index in data){
+		var itemData = data[index];
+		var itemName = itemData.name;
+		
+		var configInfo = GameUI.EconInfoConfig[itemName];
+		if(!configInfo){
+			continue;
+		}
+		
+		var slot = configInfo['slot'];
+		
+		if(slot == undefined){
+			continue;
+		}
+		var container = m_ShopLinesPanels[SHOP_TAGS[slot]];
+		if(!container){
+			continue;
+		}
+		var parent = container.FindChildTraverse("shop_items");
+		$.Msg("add " + itemName);
 
-    for (var index in data){
-        var itemData = data[index];
-        var itemName = itemData.name;
+		//m_ShopItemFromSerever_IndexByName[itemName] = itemData;
+		var itemImage = itemData.image;
 
-        //m_ShopItemFromSerever_IndexByName[itemName] = itemData;
-        var itemImage = itemData.image;
+		var itemCost = itemData.cost;
+		var itemDiscount = itemData.discount;
 
-        var itemCost = itemData.cost;
-        var itemDiscount = itemData.discount;
+		m_ShopItemPanels[itemName] = $.CreatePanel("Panel", parent, "");
+		m_ShopItemPanels[itemName].BLoadLayoutSnippet("ShopItem");
+		m_ShopItemPanels[itemName].FindChildTraverse("shop_item_title").text = $.Localize("econ_" + itemName);
 
-        m_ShopItemPanels[itemName] = $.CreatePanel("Panel", parent, "");
-        m_ShopItemPanels[itemName].BLoadLayoutSnippet("ShopItem");
-        m_ShopItemPanels[itemName].FindChildTraverse("shop_item_title").text = $.Localize("econ_" + itemName);
+		m_ShopItemPanels[itemName].FindChildTraverse("shop_item_image").SetImage("file://{resources}/images/custom_game/econ/" + itemName + ".png");
 
-        m_ShopItemPanels[itemName].FindChildTraverse("shop_item_image").SetImage("file://{resources}/images/custom_game/econ/" + itemName + ".png");
+		m_ShopItemPanels[itemName].FindChildTraverse("purchase_cost_text").SetDialogVariable("cost", String(itemCost));
+		m_ShopItemPanels[itemName].cost = itemCost;
+		// if (m_PointsLeft > 0){
+			// if (itemCost > m_PointsLeft ) {
+				// m_ShopItemPanels[itemName].enabled = false;
+				// m_ShopItemPanels[itemName].AddClass("NotEnoughPoints")
+			// }else {
+				// m_ShopItemPanels[itemName].enabled = true;
+				// m_ShopItemPanels[itemName].RemoveClass("NotEnoughPoints")
+			// }
+		// }
 
-        m_ShopItemPanels[itemName].FindChildTraverse("purchase_cost_text").SetDialogVariable("cost", String(itemCost));
-        m_ShopItemPanels[itemName].cost = itemCost;
-        // if (m_PointsLeft > 0){
-            // if (itemCost > m_PointsLeft ) {
-                // m_ShopItemPanels[itemName].enabled = false;
-                // m_ShopItemPanels[itemName].AddClass("NotEnoughPoints")
-            // }else {
-                // m_ShopItemPanels[itemName].enabled = true;
-                // m_ShopItemPanels[itemName].RemoveClass("NotEnoughPoints")
-            // }
-        // }
+		if (itemDiscount !== undefined){
+			m_ShopItemPanels[itemName].FindChildTraverse("sale_overlay").RemoveClass("DontShow");
+		}
 
-        if (itemDiscount !== undefined){
-            m_ShopItemPanels[itemName].FindChildTraverse("sale_overlay").RemoveClass("DontShow");
-        }
-
-        (function(index){
-            var itemData = data[index]
-            m_ShopItemPanels[itemData.name].SetPanelEvent("onactivate", function(){
-                ShowConfirmPurchaseDialog(itemData.name, itemData.cost);
-            });
-            m_ShopItemPanels[itemData.name].FindChildTraverse('preview_button').SetPanelEvent("onactivate", function(){
-                GameEvents.SendCustomGameEventToServer('player_preview_req', {item: itemData.name})
-                $("#page_shop").AddClass("Hidden");
-            });
-            m_ShopItemPanels[itemData.name].FindChildTraverse('equip_button').SetPanelEvent("onactivate", function(){
-                GameEvents.SendCustomGameEventToServer('player_equip_req', {to_equip: itemData.name})
-                $("#page_shop").AddClass("Hidden");
-            });
-            m_ShopItemPanels[itemData.name].SetPanelEvent("onmouseover", function(){
-                $.DispatchEvent("DOTAShowTextTooltip", m_ShopItemPanels[itemData.name], $.Localize('#description_'+itemData.name));
-            });
-            m_ShopItemPanels[itemData.name].SetPanelEvent("onmouseout", function(){
-                $.DispatchEvent("DOTAHideTextTooltip");
-            });
-        })(index);
+		(function(index){
+			var itemData = data[index]
+			m_ShopItemPanels[itemData.name].SetPanelEvent("onactivate", function(){
+				ShowConfirmPurchaseDialog(itemData.name, itemData.cost);
+			});
+			m_ShopItemPanels[itemData.name].FindChildTraverse('preview_button').SetPanelEvent("onactivate", function(){
+				GameEvents.SendCustomGameEventToServer('player_preview_req', {item: itemData.name})
+				$("#page_shop").AddClass("Hidden");
+			});
+			m_ShopItemPanels[itemData.name].FindChildTraverse('equip_button').SetPanelEvent("onactivate", function(){
+				GameEvents.SendCustomGameEventToServer('player_equip_req', {to_equip: itemData.name})
+				$("#page_shop").AddClass("Hidden");
+			});
+			m_ShopItemPanels[itemData.name].SetPanelEvent("onmouseover", function(){
+				$.DispatchEvent("DOTAShowTextTooltip", m_ShopItemPanels[itemData.name], $.Localize('#description_'+itemData.name));
+			});
+			m_ShopItemPanels[itemData.name].SetPanelEvent("onmouseout", function(){
+				$.DispatchEvent("DOTAHideTextTooltip");
+			});
+		})(index);
+	}
+	
+	for (var k in m_ShopLinesPanels) {
+		var panel = m_ShopLinesPanels[k].FindChildTraverse("shop_items");
+		if(!panel)continue;
+        if(panel.GetChildCount() > 0){
+			m_ShopLinesPanels[k].FindChildTraverse('no_good_text').style['visibility'] = 'collapse';
+		}else{
+			m_ShopLinesPanels[k].FindChildTraverse('no_good_text').style['visibility'] = 'visible';
+		}
     }
 
     RebuildShopTags();
@@ -299,7 +353,7 @@ function OnPaymentComplete(kv) {
     PaymentPanel.OnPay(OnPay);
 
     GameEvents.Subscribe("avalon_payment_complete", OnPaymentComplete);
-
+	InitTabContainers()
     QueryShopRelatedDataFromServer();
     CustomNetTables.SubscribeNetTableListener('econ_data', OnCollectionDataArrived);
     CustomNetTables.SubscribeNetTableListener('econ_data', OnShopItemsArrived);
